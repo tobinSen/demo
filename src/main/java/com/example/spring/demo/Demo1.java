5049a7fe2e0ad755e5153b7d2268fc27ca0916ac
tongjian <jian.tong@b-and-qchina.com>
2019-12-20 20:23:17 +0800

    Initial commit

---
+++ src/main/java/com/example/spring/demo/Demo1.java
@@ -0,0 +1,9 @@
+package com.example.spring.demo;
+
+public class Demo1 {
+
+
+    public void test1(String name) {
+        System.out.println(name);
+    }
+}
---
+++ src/main/java/com/example/spring/demo/Demo2.java
@@ -0,0 +1,16 @@
+package com.example.spring.demo;
+
+import java.util.Date;
+
+public class Demo {
+
+    private Long id;
+    private String name;
+
+    private String gender;
+    private Date date;
+
+    public void test(String name){
+        System.out.println(name);
+    }
+}
