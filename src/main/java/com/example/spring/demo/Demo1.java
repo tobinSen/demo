d24ced3fad2a283e0e45b22e0d89590abb3b0533
tongjian <jian.tong@b-and-qchina.com>
2019-12-20 20:52:16 +0800

    init-test

--- src/main/java/com/example/spring/demo/Demo1.java
+++ src/main/java/com/example/spring/demo/Demo1.java
@@ -8,12 +8,12 @@ public class Demo1 {
     }
 
     public void test1(String name, Long age) {
-        System.out.println(name + age+"test");
+        System.out.println(name + age+"master");
     }
     public void test2(String name, Long age) {
-        System.out.println(name + age+"test");
+        System.out.println(name + age+"dev");
     }
     public void test3(String name, Long age) {
-        System.out.println(name + age+"test");
+        System.out.println(name + age+"dev");
     }
 }
