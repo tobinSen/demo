ca01ba7d89ebd6411eca483e82d24cc3db9089be
tongjian <jian.tong@b-and-qchina.com>
2019-12-20 20:42:09 +0800

    init-dev

--- src/main/java/com/example/spring/demo/Demo1.java
+++ src/main/java/com/example/spring/demo/Demo1.java
@@ -2,6 +2,8 @@ package com.example.spring.demo;
 
 public class Demo1 {
 
+    private Long idDeve;
+    private Long String ;
 
     public void test1(String name) {
         System.out.println(name);
@@ -11,9 +13,16 @@ public class Demo1 {
         System.out.println(name + age+"master");
     }
     public void test2(String name, Long age) {
-        System.out.println(name + age+"master");
+        System.out.println(name + age+"dev");
     }
     public void test3(String name, Long age) {
-        System.out.println(name + age+"master");
+        System.out.println(name + age+"dev");
     }
+    /**
+     * 常见情况：
+     *  1.merge后commit时，显示改变的会存在之前旧的显示
+     *  2.merge后就没有，
+     *  3.少merger多 多merge少
+     *
+     */
 }
